5cbc76c71f932184534d75e88573f9a0e90dd7d9
Michael Herrmann <herrmann@neoskop.de>
2023-03-28 14:33:34 +0200

    first commit

---
+++ src/react-bricks/bricks/index.ts
@@ -0,0 +1,9 @@
+import { types } from 'react-bricks/frontend'
+import HeroUnit from './MyHeroUnit'
+
+const bricks: types.Brick<any>[] = [
+  // Put here your other bricks...
+  HeroUnit, // Example brick
+]
+
+export default bricks
